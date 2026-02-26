1ed7edb04ea64fcbbfde70a97a2bd705be2302c8
Mohammad Aamir <aamireverlasting786>
2026-02-27 00:50:44 +0530

    added controller

--- src/routes/identify.route.ts
+++ src/routes/identify.route.ts
@@ -1,16 +1,8 @@
 import { Router } from "express";
-import { identifyContact } from "../services/identify.service";
+import { identifyHandler } from "../controllers/identify.controller";
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-    try {
-        const { email, phoneNumber } = req.body;
-        const result = await identifyContact(email, phoneNumber);
-        res.status(200).json({ contact: result });
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post("/", identifyHandler);
 
 export default router;
